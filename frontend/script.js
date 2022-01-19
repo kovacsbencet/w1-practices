e5861c68c4d2cb1aa7fb56bcc33db0c9af6e397f
kovacsbencet <kovacsbencet@gmail.com>
2022-01-19 21:40:39 +0100

    data structure added

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,54 @@
+/* const data = [
+    "Gergely Kiss",
+    "Krisztián Bui",
+    "Buczkó Balázs",
+    "András Varga",
+    "Borbála Teréz Kovács"
+];
+ */
+
+const data = [
+    {
+        name: "Gergely Kiss",
+        favouriteFood: "csirke/rizs"
+    },
+    {
+        name: "András Varga",
+        favouriteFood: "brassói"
+    },
+    {
+        name: "Buczkó Balázs",
+        favouriteFood: "kolbászos rájás pizza"
+    },
+    {
+        name: "Krisztián Bui",
+        favouriteFood: "banán"
+    },
+    {
+        name: "Borbála Teréz Kovács",
+        favouriteFood: "avokádó"
+    },
+    {
+        name: "Benett Viszokai",
+        favouriteFood: "alma"
+    },
+    {
+        name: "Krisztián Pörneczi",
+        favouriteFood: "almáspite"
+    },
+]
 function LoadEvent() {
     console.log("Az oldal betöltődött.");
 
-    const list = document.querySelectorAll("section");
+    const root = document.querySelector("#root")
 
-    for (const elem of list) {
-        elem.insertAdjacentHTML("beforeend", `
-        <p>text</p>
-        `);
-        //elem.querySelector("h1").outerHTML = ""
+    for (const frontendStudent of data) {
+        root.insertAdjacentHTML("beforeend", `
+            <section>
+                <h1>${frontendStudent.name}</h1>
+                <p>${frontendStudent.favouriteFood}</p>
+            </section>
+        `)
     }
 }
-window.addEventListener("load", LoadEvent)
+window.addEventListener("load", LoadEvent);
