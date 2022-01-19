e5eaad5b8e2c77cfaba66eb797c7bb9debe21caa
kovacsbencet <kovacsbencet@gmail.com>
2022-01-19 16:48:32 +0100

    getting familiar with html, css and javascript

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+function LoadEvent() {
+    console.log("Az oldal betöltődött.");
+
+    const list = document.querySelectorAll("section");
+
+    for (const elem of list) {
+        elem.insertAdjacentHTML("beforeend", `
+        <p>text</p>
+        `);
+        //elem.querySelector("h1").outerHTML = ""
+    }
+}
+window.addEventListener("load", LoadEvent)
