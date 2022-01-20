c4f369513b1acd6d45fce6fc4ccc07d8855faadf
kovacsbencet <kovacsbencet@gmail.com>
2022-01-20 15:13:19 +0100

    I imported some data.

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,25 @@
-function LoadEvent() {
+
+
+async function loadEvent() {
     console.log("Az oldal betöltődött.");
 
-    const list = document.querySelectorAll("section");
+    const root = document.querySelector("#root");
 
-    for (const elem of list) {
-        elem.insertAdjacentHTML("beforeend", `
-        <p>text</p>
-        `);
-        //elem.querySelector("h1").outerHTML = ""
+    const countryRes = await fetch("https://restcountries.com/v3.1/all");
+    const countryArr = await countryRes.json();
+
+    //console.log(countryArr[0].name.common)
+
+    for (const country of countryArr) {
+
+        //console.log(country.name.common);
+
+        root.insertAdjacentHTML("beforeend", `
+            <section>
+                <h1>${country.name.common}</h1>
+                <p>${country.region}</p>
+            </section>
+        `)
     }
 }
-window.addEventListener("load", LoadEvent)
+window.addEventListener("load", loadEvent)
